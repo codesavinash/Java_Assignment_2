e4ae565a9784d35e429d0c7736502a14b651fd23
Avinash sreedharan <Avinash.Sreedharan762@triosstudent.com>
2025-07-27 17:16:02 -0400

    chicken subclass extending animal

--- animal/Chicken.java
+++ animal/Chicken.java
@@ -1,5 +1,19 @@
 package animal;
 
-public class Chicken {
-    
-}
+public class Chicken extends Animal {
+  private String eggColor;
+
+  // constructor
+  public Chicken(String eggColor) {
+    super("Chicken");
+    this.eggColor = eggColor;
+  }
+  // getter
+  public String getEggColor() {
+    return eggColor;
+  }
+  // setter
+  public void setEggColor(String eggColor) {
+    this.eggColor = eggColor;
+  }
+} // class
